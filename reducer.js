6e0cdd64a9e10d62642dfa5a4650336c6daa9938
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-02 16:12:01 +0300

    Add files via upload

--- reducer.js
+++ reducer.js
@@ -8,6 +8,9 @@ function reducer(state, action){
     if(action.type === 'Add_Name'){
         return [...state, action.payload]
     }
+    if(action.type === 'Delete_Name'){
+        return state.filter(personName => personName !== action.payload);
+    }
     return state;
 }
 
@@ -15,4 +18,7 @@ let newState =  reducer(initialState, {type: 'Add_Name', payload: 'Dima'});
 console.log(newState);
 
 newState =  reducer(newState, {type: 'Add_Name', payload: 'Alice'});
+console.log(newState);
+
+newState =  reducer(newState, {type: 'Delete_Name', payload: 'Alice'});
 console.log(newState);
